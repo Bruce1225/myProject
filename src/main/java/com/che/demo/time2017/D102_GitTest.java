b99e0384e2cee0b2dbfacd2e552e5ff7b9828e51
Che <cfbdyx@126.com>
2017-06-30 13:30:47 +0800

    test git in riz

--- src/main/java/com/che/demo/time2017/D102_GitTest.java
+++ src/main/java/com/che/demo/time2017/D102_GitTest.java
@@ -3,6 +3,8 @@ package com.che.demo.time2017;
 public class D102_GitTest {
 	public static void main(String[] args) {
 		System.out.println("commit in home delete ssh  add_back ssh  ");
+
+		System.out.println("commit in riz ");
 	}
 
 }
