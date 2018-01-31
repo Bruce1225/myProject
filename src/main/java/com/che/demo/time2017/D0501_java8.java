a580326e29c10cd58a04f9bc85238f196ce320ce
Bruce <CheFB_2014@163.com>
2018-01-31 20:34:15 +0800

    fix

--- src/main/java/com/che/demo/time2017/D0501_java8.java
+++ src/main/java/com/che/demo/time2017/D0501_java8.java
@@ -12,6 +12,11 @@ public class D0501_java8 {
 		l.add("b222");
 		l.forEach(e -> System.out.println( e ));
 		Arrays.asList( "a", "b", "d" ).forEach( e -> System.out.println( e ) );
+		
+		int a1[] = {1};
+		String a2[] = {"",""};
+		String a3[] = new String[3];
+		int[][] a4 = new int[3][3];
 	}
 
 }
